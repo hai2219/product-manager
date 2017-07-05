30c173d8c0d37fc4d4af9f0e372a4685a1357d02
Hai Ngo <haint@nexlesoft.com>
2017-07-05 13:14:41 +0700

    remove old project

--- src/config.js
+++
@@ -1,8 +0,0 @@
-import helpers from './helpers'
-export default {
-  bucket: {
-    slug: helpers.getParameterByName('bucket_slug'),
-    read_key: helpers.getParameterByName('read_key'),
-    write_key: helpers.getParameterByName('write_key')
-  }
-}
